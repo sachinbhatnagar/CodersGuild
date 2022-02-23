8549b42e7d35ef58a2f055998dc6433a76b68a24
LloydRivers <lloydrivers58@hotmail.com>
2022-02-23 13:51:44 +0000

    Added React Bootsrap button

--- React Challenges/to-do/src/App.js
+++ React Challenges/to-do/src/App.js
@@ -1,23 +1,37 @@
 import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
 import "./App.css";
 
 function App() {
+  const date = new Date();
+  console.log(date);
   const [events, setEvents] = useState([]);
   const [input, setInput] = useState("");
   const handleInput = (e) => {
     setInput(e.target.value);
   };
   const handleSubmit = (e) => {
-    setEvents([...events, input]);
+    const inputAndDate = {
+      date: new Date(Date.now()),
+      input,
+    };
+    setEvents([...events, inputAndDate]);
   };
   return (
     <div className="App">
       <input value={input} onChange={handleInput} type="text" />
-      <button onClick={handleSubmit}>submit</button>
+      <Button className="btn" onClick={handleSubmit}>
+        submit
+      </Button>
       <div>
         <ul>
           {events.map((event, index) => {
-            return <li key={index}>{event}</li>;
+            return (
+              <li key={index}>
+                {event.input} {event.date.toDateString()}
+              </li>
+            );
           })}
         </ul>
       </div>
