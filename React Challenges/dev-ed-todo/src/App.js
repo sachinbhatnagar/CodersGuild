a53120703cbfebac7c4d2f9f70f13ba210c9952a
LloydRivers <lloydrivers58@hotmail.com>
2022-02-24 11:04:40 +0000

    removed pngs

--- React Challenges/dev-ed-todo/src/App.js
+++ React Challenges/dev-ed-todo/src/App.js
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 import Form from "./components/Form";
+import ToDoList from "./components/ToDoList";
 
 function App() {
+  const [inputText, setInputText] = useState("");
   return (
     <div className="App">
       <header>Task Tracker</header>
-      <Form />
+      <Form setInputText={setInputText} />
+      <ToDoList />
     </div>
   );
 }
 
 export default App;
+
+function foo(arr) {}
+
+foo([1, 2, 4]);
