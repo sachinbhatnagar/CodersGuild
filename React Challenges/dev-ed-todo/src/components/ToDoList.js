a8460fb2bdea0f5afa76419d06394988ee87146a
LloydRivers <lloydrivers58@hotmail.com>
2022-02-25 08:55:39 +0000

    Added delete functionality.

--- React Challenges/dev-ed-todo/src/components/ToDoList.js
+++ React Challenges/dev-ed-todo/src/components/ToDoList.js
@@ -1,13 +1,24 @@
 import React from "react";
 import Todo from "./Todo";
 
-function ToDoList({ todos }) {
+function ToDoList({ todos, setTodos }) {
+  console.log(todos, "todos");
+  console.log(setTodos, "setTodos");
   return (
     <div className="todo-container">
       <ul className="todo-list">
-        {todos.map((todo) => (
-          <Todo key={todo.id} text={todo.text} id={todo.id} />
-        ))}
+        {todos.map((todo) => {
+          console.log(todo);
+          return (
+            <Todo
+              setTodos={setTodos}
+              todos={todos}
+              key={todo.id}
+              text={todo.text}
+              id={todo.id}
+            />
+          );
+        })}
       </ul>
     </div>
   );
--- React Challenges/dev-ed-todo/src/components/Todo.js
+++ React Challenges/dev-ed-todo/src/components/Todo.js
@@ -1,13 +1,18 @@
 import React from "react";
 
-function Todo({ text }) {
+function Todo({ text, todos, setTodos, id }) {
+  const deleteHandler = () => {
+    console.log("Hello ");
+    setTodos(todos.filter((el) => el.id != id));
+  };
+
   return (
     <div className="todo">
       <li className="todo-item">{text}</li>
       <button className="complete-btn">
         <i className="fas fa-check"></i>
       </button>
-      <button className="trash-btn">
+      <button onClick={deleteHandler} className="trash-btn">
         <i className="fas fa-trash"></i>
       </button>
     </div>
