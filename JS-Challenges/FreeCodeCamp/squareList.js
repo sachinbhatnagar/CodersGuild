d8a25b91e2fa134a70d8207df20a06327fc82766
LloydRivers <lloydrivers58@hotmail.com>
2022-03-06 20:31:30 +0000

    squareList.js

---
+++ JS-Challenges/FreeCodeCamp/squareList.js
@@ -0,0 +1,14 @@
+const squareList = (arr) => {
+  const positives = arr.filter((num) => {
+    if (num > 0 && Number.isInteger(num)) {
+      return num;
+    } else {
+      return false;
+    }
+  });
+  const squared = positives.map((num) => {
+    return num * num;
+  });
+  return squared;
+};
+const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
