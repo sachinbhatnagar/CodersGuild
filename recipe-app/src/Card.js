335e24ca9d4128b4a5cc416ecf438f7107a510ef
LloydRivers <lloydrivers58@hotmail.com>
2022-03-20 18:33:03 +0000

    recipe app
    recipe app

---
+++ recipe-app/src/Card.js
@@ -0,0 +1,25 @@
+import React from "react";
+//comment
+function Card({ image, healthLabels, label, cuisineType, ingredientLines }) {
+  return (
+    <div className="card">
+      <img className="card-img-top" src={image} alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">{label}</h5>
+        <p className="card-text">{cuisineType}</p>
+      </div>
+      <ul className="list-group list-group-flush">
+        {ingredientLines.map((ingredientLine, index) => {
+          return <li key={index}>{ingredientLine}</li>;
+        })}
+      </ul>
+      <div className="card-body">
+        {healthLabels.slice(0, 5).map((healthLabel, index) => {
+          return <span key={index}>{healthLabel}</span>;
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Card;
